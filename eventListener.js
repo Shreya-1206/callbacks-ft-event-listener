2828eed654c375cc909ffbb49b48671eac373b3e
Shreya <shreyaray.1206@gmail.com>
2024-05-20 17:02:30 +0530

    callback and event listener

--- eventListener.js
+++ eventListener.js
@@ -1,3 +1,11 @@
-document.getElementById("btn").addEventListener("click", function click(){
-    console.log("Button Clicked !!")
-});
+//count the button clicked times
+
+function addClickCount() {
+    let count = 0
+    document.getElementById("btn").addEventListener("click", function click(){
+        console.log("Button Clicked !!", ++count);
+    });  
+}
+
+addClickCount();
+
